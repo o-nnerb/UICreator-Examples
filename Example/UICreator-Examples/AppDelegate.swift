a70a976d7f7e4bffc45a4d6c1b7e920171f004b7
brennoumobi <brenno@umobi.com.br>
2020-01-15 12:45:47 -0300

    Project is now public

--- Example/UICreator-Examples/AppDelegate.swift
+++ Example/UICreator-Examples/AppDelegate.swift
@@ -17,14 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+
         let window = UIWindow(frame: UIScreen.main.bounds)
         window.rootViewController = ContainerController(Host {
             ContentView()
         })
         window.makeKeyAndVisible()
         self.window = window
-        
+
         return true
     }
 
