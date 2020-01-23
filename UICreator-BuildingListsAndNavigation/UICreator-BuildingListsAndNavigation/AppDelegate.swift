bff3600da98b50f30a369e3375013f4065dbd054
brennoumobi <brenno@umobi.com.br>
2020-01-23 15:10:15 -0300

    Break into different examples

---
+++ UICreator-BuildingListsAndNavigation/UICreator-BuildingListsAndNavigation/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  Teste1
+//
+//  Created by brennobemoura on 18/12/19.
+//  Copyright © 2019 brennobemoura. All rights reserved.
+//
+
+import UIKit
+import UICreator
+import UIContainer
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = ContainerController(UICHost {
+            ContentView()
+        })
+        window.makeKeyAndVisible()
+        self.window = window
+
+        return true
+    }
+}
+
---
+++ UICreator-BuildingListsAndNavigation/UICreator-BuildingListsAndNavigation/CircleImage.swift
@@ -0,0 +1,39 @@
+//
+//  CircleImage.swift
+//  UICreator-Examples_Example
+//
+//  Created by brennobemoura on 21/01/20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import UICreator
+
+class CircleImage: UICView {
+    let image: UIImage
+
+    init(image: UIImage) {
+        self.image = image
+        super.init()
+    }
+
+    var body: ViewCreator {
+        UICSpacer {
+            UICCircle {
+                UICImage(image: self.image)
+                    .aspectRatio()
+                    .content(mode: .scaleAspectFill)
+            }.border(width: 4)
+            .border(color: .white)
+        }.shadow(radius: 10)
+        .shadow(ocupacity: 0.3)
+    }
+}
+
+import SwiftUI
+
+struct CircleImage_Preview: PreviewProvider {
+    static var previews: some View {
+        LivePreview(CircleImage(image: landmarkData[0].image))
+    }
+}
